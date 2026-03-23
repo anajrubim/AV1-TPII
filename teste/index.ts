df37744a7a9b8ba49de292ea216f4e5d16debfae
Ana Júlia Rubim <anajrbcosta25@gmail.com>
2026-03-23 13:49:11 -0300

    Aplicando o padrão protótipo

--- teste/index.ts
+++ teste/index.ts
@@ -1,10 +1,13 @@
 import Cliente from "../modelos/cliente";
 import Endereco from "../modelos/endereco";
+import Telefone from "../modelos/telefone";
+
 let cliente = new Cliente()
 cliente.nome = `Pedro de Alcântara João Carlos Leopoldo Salvador`
 cliente.nomeSocial = `Dom Pedro II`
 cliente.dataCadastro = new Date(1840, 6, 23)
 cliente.dataNascimento = new Date(1825, 11, 2)
+
 let endereco = new Endereco()
 endereco.rua = `R. do Catete`
 endereco.bairro = `Copacabana`
@@ -14,12 +17,16 @@ endereco.pais = `Brasil`
 endereco.codigoPostal = `22220-000`
 cliente.endereco = endereco
 
+let telefone = new Telefone("21", "99999-9999")
+cliente.telefone = telefone
+
 let dependente = new Cliente()
 dependente.nome = `Isabel Cristina Leopoldina Augusta Micaela`
 dependente.nomeSocial = `Princesa Isabel`
 dependente.dataCadastro = new Date(1921, 10, 14)
 dependente.dataNascimento = new Date(1846, 6, 29)
 dependente.endereco = (cliente.endereco.clonar() as Endereco)
+dependente.telefone = cliente.telefone.clonar()
 dependente.titular = cliente
 cliente.dependentes.push(dependente)
 
